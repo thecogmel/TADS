1292de3f592f72407d9b5273db753e9d4872c79b
TheCogmel <erick_mmedeiros@hotmail.com>
2020-02-19 16:31:17 -0300

    teste 1

--- script1.cpp
+++ script1.cpp
@@ -5,5 +5,25 @@ int main (){
 
     /* comentário
     de
-    barra*/
+    bloco*/
+
+    int N, Z; //N: pen, Z: cash, Y: change
+    float Y; // float is used for real numbers
+    float custoCaneta;
+
+    //READ and Write
+    std::cout << "N: ";
+    std::cout << "Z: ";
+    std::cout << "Y: ";
+    std::cin >> N >> Z >> Y;
+
+    //TEST
+    if (Z>0 and N>0 and Z>Y and Y>=0) {
+        custoCaneta = (Z-Y)/N;
+        std::cout << "Custo da Caneta:" << custoCaneta;
+    } else std::cout << "algum valor invalido";
+
+    
+    
+
 }

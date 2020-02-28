6af75572fffa7cd2b5f7ab81fa2af906c9dfb667
Erick Medeiros <erick_mmedeiros@hotmail.com>
2020-02-28 10:32:14 -0300

    aviao.cpp corrigido sem check function

--- aviao.cpp
+++ aviao.cpp
@@ -2,5 +2,30 @@
 
 int main (){
     
-    std::cout << "Olá mundo\n";
+    int C,P,F, result;
+
+    //std::cout << "C: ";
+    std::cin >> C;
+    //std::cout << "P: ";
+    std::cin >> P;
+    //std::cout << "F: ";
+    std::cin >> F;
+
+
+       if (1 <= C <= 1000 && 1 <= P <= 1000 && 1 <= F <= 1000)
+    {
+        result = (P/C);
+        //std::cout << "result = " << result << "\n";
+        if (F > result)
+        {
+            std::cout << "N";
+        } else
+        {
+            std::cout << "S";
+        }          
+    }   else
+    {
+        std::cout << "entrada invalida";
+    }
+    
 }

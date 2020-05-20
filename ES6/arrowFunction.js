c1408154f00f7dff8babb84d19ecdebd16001e3b
Erick Medeiros <erick_mmedeiros@hotmail.com>
2020-05-20 17:32:57 -0300

    arrowFunction lesson item => unicode

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,11 @@
+var arr = [1, 2, 3, 4, 5, 8, 9];
+
+const newArr = arr.map (item => item * 2);
+
+console.log(newArr);
+
+const teste = () => ({nome: 'Erick'})
+
+console.log (teste());
+
+/* arrow function   (parametro => algoritmo de retorno unico) */

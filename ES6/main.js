5ed9011fb66955e89a3406a38bbc3583eff42e03
Erick Medeiros <erick_mmedeiros@hotmail.com>
2020-05-20 16:50:30 -0300

    class lesson

--- ES6/main.js
+++ ES6/main.js
@@ -1,7 +1,28 @@
-alert ('teste');
-
-class Teste {
-    metodo (){
+class List {
+    constructor () {
+        this.data = [];
 
     }
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
 }
+class TodoList extends List{
+    constructor () {
+        super();
+        this.usuario = 'Erick';
+    }
+    
+    mostraUsuario (){
+        console.log(this.usuario)
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.add('Novo Todo');
+}
+
+MinhaLista.mostraUsuario ();

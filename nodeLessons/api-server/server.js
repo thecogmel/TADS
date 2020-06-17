8e9254596a71f80bfdbb73636188221780cc9287
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-06-17 17:57:37 -0300

    add cors dependencies

--- nodeLessons/api-server/server.js
+++ nodeLessons/api-server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir'); //  require todos os models da pasta
 
 //Iniciando o App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect('mongodb+srv://erick:erick@api-clusters-0sqru.mongodb.net/API?retryWrites=true&w=majority',{

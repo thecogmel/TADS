8f8ee71e82b4c27024bade30200577dcb4d02440
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-09-23 15:54:54 -0300

    back to code

--- react/huntweb/src/App.js
+++ react/huntweb/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+//import api from './services/api'
+import './style.css';
+import Header from './components/Header'
+import Main from "./pages/main"
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello  World!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+
+)
 
 export default App;

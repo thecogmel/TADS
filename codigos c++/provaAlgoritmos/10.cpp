a39116a8182e7cf99e23ee1692854f3260646cd0
Erick Medeiros <erickmedeiros@ufrn.edu.br>
2020-11-18 14:21:00 -0300

    fix question 10 routes problem

--- codigos c++/provaAlgoritmos/10.cpp
+++ codigos c++/provaAlgoritmos/10.cpp
@@ -4,19 +4,21 @@ using namespace std;
 int km(int tabela[6][6], int rota[2][6])
 {
 
-    int linha, coluna, result = 0, l, c;
+    int linha, coluna, result = 0, l=0, c=0;
 
     for (coluna = 0; coluna < 6; coluna++) //for com valores invertidos para ler coluna/linha
     {   
         for (linha = 0; linha < 2; linha++)
         {
-            if (linha == 0)
+            if (coluna == 0)
             {
-                l = rota[linha][coluna]; //se na primeira posição, apenas atribui a linha
+                l = c;
+                c = rota[linha][coluna];
             }
-            if (linha == 1)
+            if (linha != 0 and coluna != 0)
             {
-                c = rota[linha][coluna]; // se na segunda atribui a coluna e faz o cálculo da rota
+                l = c;
+                c = rota[linha][coluna];
                 result = result + tabela[l-1][c-1];
             }
         }
@@ -29,7 +31,7 @@ int main(int argc, char const *argv[])
     int l, c;
     int tabela[6][6] = {{0, 63, 210, 190, 0, 190},
                         {63, 0, 160, 150, 95, 0},
-                        {210, 160, 0, 10, 0, 10},
+                        {210, 160, 0, 10, 0, 0},
                         {190, 150, 10, 0, 0, 0},
                         {0, 95, 0, 0, 0, 80},
                         {190, 0, 0, 0, 80, 0}};

e313221fac39110f5da67148f2782347df7c0f61
Erick Medeiros <erick_mmedeiros@hotmail.com>
2021-06-14 16:04:43 -0300

    git refresh

---
+++ react/huntweb/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import api from './services/api'
+import './style.css';
+import Header from './components/Header'
+import Main from "./pages/main"
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+
+)
+
+export default App;
